d9e7a7ffd5e1e5ecb246489bd2a6966441ae2b11
abprash <panuradh@buffalo.edu>
2017-08-19 18:45:10 -0400

    added one away

--- src/main/java/stringsAndArrays/StringAndArrayLauncher.java
+++ src/main/java/stringsAndArrays/StringAndArrayLauncher.java
@@ -15,9 +15,10 @@ public class StringAndArrayLauncher {
 			testWords[i] = s1.charAt(i);
 		}
 		//urlify2(testWords);
-		String temp = "taco cat";
-		String temp2 = "Taco cat";
-		System.out.println(palindromePermutation1(temp2));
+		String temp = "cat";
+		String temp2 = "";
+		//System.out.println(palindromePermutation1(temp2));
+		System.out.println(oneAway1(temp, temp2));
 	}
 	
 	//Problem 1.1
@@ -234,6 +235,49 @@ public class StringAndArrayLauncher {
 		}
 		return true;
 		
+	}
+	
+	public static boolean oneAway1(String s1, String s2){
+		//validate s1 and s2
+		if(s1 == null || s2 == null)
+			return false;
+		if(Math.abs(s1.length() - s2.length()) >1)
+			return false;
+		int[] countArray = new int[52];
+		for(int i=0; i<s1.length(); i++){
+			char ch = s1.charAt(i);
+			int pos = 0;
+			if(Character.isLetter(ch)){
+				if(Character.isUpperCase(ch))
+					pos = ch - 'A';
+				else
+					pos = ch - 'a' + 26;
+				if((pos > countArray.length-1)||(pos < 0) )
+					return false;//unidentified char
+				countArray[pos]++;
+			}
+		}
+		
+		for(int i=0; i<s2.length(); i++){
+			char ch = s2.charAt(i);
+			int pos = 0;
+			if(Character.isLetter(ch)){
+				if(Character.isUpperCase(ch))
+					pos = ch - 'A';
+				else
+					pos = ch - 'a' + 26;
+				if((pos > countArray.length-1)||(pos < 0) )
+					return false;//unidentified char
+				countArray[pos]--;
+			}
+		}
+		int sum = 0;
+		for(int i=0; i<countArray.length ; i++){
+			sum +=countArray[i];
+		}
+		if(sum > 1 || sum < -1)
+			return false;
+		return true;
 	}
 	
 
