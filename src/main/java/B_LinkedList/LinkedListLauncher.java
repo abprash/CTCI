4c4da3640b70ccb58f5123116a92cc1b48afabe5
abprash <panuradh@buffalo.edu>
2017-09-11 00:40:59 -0400

    working on remove dups without extra space

--- src/main/java/B_LinkedList/LinkedListLauncher.java
+++ src/main/java/B_LinkedList/LinkedListLauncher.java
@@ -17,8 +17,9 @@ public class LinkedListLauncher {
 		linkedList5.append(3);
 		linkedList5.traversal();
 		//linkedList5.deleteNodesHavingBiggerRightValue();
-		linkedList5.traversal();
-		System.out.println(removeDups(linkedList5));
+		//linkedList5.traversal();
+		//System.out.println(removeDups(linkedList5));
+		removeDupsWithNoExtraSpace(linkedList5);
 	}
 	//CTCI Linked list problems
 	
@@ -27,23 +28,41 @@ public class LinkedListLauncher {
 			return null;
 		MyLinkedList.Node temp = list.head;
 		HashSet<Integer> set = new HashSet<Integer>();
-		while(temp.next != null){
+		while(temp != null){
 			set.add(temp.data);
 			temp = temp.next;
 		}
 		return set;
 	}
 	
-	public static HashSet<Integer> removeDupsWithNoExtraSpace(MyLinkedList list){
+	public static MyLinkedList removeDupsWithNoExtraSpace(MyLinkedList list){
 		if(list == null)
 			return null;
-		MyLinkedList.Node temp = list.head;
-		HashSet<Integer> set = new HashSet<Integer>();
-		while(temp.next != null){
-			set.add(temp.data);
-			temp = temp.next;
+		MyLinkedList.Node p1 = list.head;
+		if(p1.next == null)
+			return list;
+		MyLinkedList.Node p2 = p1.next;
+		if(p2.next == null && p1.data == p2.data)
+			return list;
+		MyLinkedList.Node p3 = p2.next;
+		while(p1 != null){
+			while(p3 != null){
+				if(p1.data != p3.data){
+					p2 = p2.next;
+					p3 = p3.next;
+				}
+				else{ 
+					if(p3.next != null)
+						p2.next = p3.next;
+				else
+					p2.next = null;
+				p3 = p3.next;
+				}
+			}
+			p1 = p1.next;
 		}
-		return set;
+		list.traversal();
+		return list;
 	}
 	
 	
